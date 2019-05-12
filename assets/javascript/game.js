3dddd1b8215b045598bc6b9db02baaf07616a083
Sherard <sherardg@pennmedicine.upenn.edu>
2019-05-11 23:42:20 -0400

    updates to html, css, and javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,29 @@
+    // Create an array of words to be guessed
 
-        // Create variables that hold references to the places in the HTML where we want to display things.
+    var hiphopArtist = [
+        "run dmc",
+        "big daddy kane",
+        "krs-one",
+        "biggie smalls",
+        "nas",
+        "snoop dogg",
+        "wu tang clan",
+        "mc lyte",
+        "naughty by nature",
+        "a tribe called quest"
+    ]; 
 
-        var wins = document.getElementById("wins");
-        var currentWord = document.getElementById("currentWord");
-        var remainingGuesses = document.getElementById("remainingGuesses");
-        var alreadyGuessed = document.getElementById("alreadyGuessed");
-        
-        // Create an array of words to be guessed
+    //Global variables
 
-        var hiphopArtist = [
-                "run dmc",
-                "big daddy kane",
-                "krs-one",
-                "biggie smalls",
-                "nas",
-                "snoop dogg",
-                "wu tang clan",
-                "mc lyte",
-                "naughty by nature",
-                "a tribe called quest"
-        ];
-
+        var wins = 0;
+        var currentWord;
+        var guessedLetters = "_";
+        var remainingGuesses = 0;
+        var alreadyGuessed = 0;
+        var gameStarted = false;
+        var hasFinished = false;
+        var maxNum = 10;
+       
         //Pick a random workd from the hiphopArtist array
 
         var words = hiphopArtist[Math.floor(Math.random() * hiphopArtist.length)];
@@ -28,36 +31,44 @@
         //Create an empty array for random words
         var answerArray =[];
 
-        for (var i = 0; i < words.length; i++){
-            answerArray[i] = "_";
+        for (var i = 0; i < words[currentWord].length; i++){
+            guessedLetters.push("_");
         }
 
-        //Create a variable to hold the number of remabiningLetters to be guessed
-        var remainingGuesses = word.length;
+    // Create variables that hold references to the places in the HTML where we want to display things.
+
+    var wins = document.getElementById("wins");
+    var currentWord = document.getElementById("currentWord");
+    var remainingGuesses = document.getElementById("remainingGuesses");
+    var alreadyGuessed = document.getElementById("alreadyGuessed");
+
+    // Updates the display in the html pge
+
+    function updateDisplay() {
+        document.getElementById("wins").innerText = wins;
+        document.getElementById("currentWord").innerText = "";
+        for (var i = 0; i < guessedLetters.length; i++){
+            document.getElementById("currentWord").innerText += guessedLetters[i];
+        }
+    }
+
+    // This function is run whenever the user presses a key.
+    document.onkeyup = function(event) {
+
+    //Determines which key was pressed.
+      currentWord.textContent = event.key;
+
+
+
+    //Create a variable to hold the number of remabiningLetters to be guessed
+       var remainingGuesses = word.length;
 
         // ****This is where Game Loop begins ****
 
-        while (remainingGuesses > 0) {
-              //Show progress to player
-            alert(answerArray.join(" "));
-    } 
-
-        // This function is run whenever the user presses a key.
-    document.onkeyup = function(event) {
-
-        // Determines which key was pressed.
-        currentWord.textContent = event.key;
-    }
-      
-
-       
-
-
-        
-        //Game Functions
-
-
-
+     while (remainingGuesses > 0) {
+     //Show progress to player
+    alert(answerArray.join(" "));
+     } 
 
         // Use Math.floor to round the random number down to the nearest whole.
         currentWordIndex = Math.floor(Math.random() * (hiphopArtist.length));
